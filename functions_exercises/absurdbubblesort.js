b5181da02ef17d020d63b8c2cff02143f42008cc
Tu Ngo <tungo8810@gmail.com>
2017-04-27 14:37:31 -0700

    Complete absurdBubbleSort method

---
+++ functions_exercises/absurdbubblesort.js
@@ -0,0 +1,59 @@
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function askIfGreaterThan(el1, el2, callback) {
+  reader.question(`Is ${el1} > ${el2}: `, (answer) => {
+    if (answer === 'yes') {
+      callback(true);
+    } else {
+      callback(false);
+    }
+  });
+}
+
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+  if (i < arr.length - 1) {
+    askIfGreaterThan(arr[i], arr[i + 1], (isGreaterThan) => {
+      if (isGreaterThan) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        madeAnySwaps = true;
+      }
+      innerBubbleSortLoop(arr, i + 1, madeAnySwaps, outerBubbleSortLoop);
+    });
+  } else {
+    outerBubbleSortLoop(madeAnySwaps);
+  }
+}
+
+function absurdBubbleSort(arr, sortCompletionCallback) {
+  function outerBubbleSortLoop(madeAnySwaps) {
+    if (madeAnySwaps === true) {
+      innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+    } else {
+      sortCompletionCallback(arr);
+    }
+  }
+
+  outerBubbleSortLoop(true);
+}
+
+absurdBubbleSort([5,2,7,1,3,6,4], (arr) => {
+  console.log(arr);
+  reader.close();
+});
+
+
+
+// askIfGreaterThan(1, 6, (answer) => {
+//   if (answer) {
+//     console.log(`1 > 6`);
+//   } else {
+//     console.log("1 < 6");
+//   }
+// });
+
+// innerBubbleSortLoop([1, 3, 2], 0, false, () => console.log('outer'));
