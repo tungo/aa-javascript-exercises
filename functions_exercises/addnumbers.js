0394edfe8d516fcfb253f4b87c6c7b0308c26bb1
Tu Ngo <tungo8810@gmail.com>
2017-04-27 13:51:01 -0700

    Complete addNumbers function

---
+++ functions_exercises/addnumbers.js
@@ -0,0 +1,23 @@
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function addNumbers(sum, numsLeft, completionCallback) {
+  if (numsLeft > 0) {
+    reader.question("What number do you wanna add?", (answer) => {
+      let number = parseInt(answer);
+      sum += number;
+
+      console.log(sum);
+      addNumbers(sum, numsLeft - 1, completionCallback);
+    });
+  } else {
+    reader.close();
+    completionCallback(sum);
+  }
+}
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
